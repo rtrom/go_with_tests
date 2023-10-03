e27e547605348af8799c990c7e96fa51bec3aab5
rtrom <rhett.trom@pm.me>
2023-10-03 11:56:08 -0500

    First commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module clockface
+module learn_go_with_tests
 
 go 1.21.1
---
+++ reflection/reflection.go
@@ -0,0 +1,38 @@
+package main
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
